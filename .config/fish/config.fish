f8b5de1fc5d1e78effd10b3a979366f380872b2e
Simon Plakolb <s.plakolb@gmail.com>
2021-06-16 11:16:34 +0200

    fish: Fix android studio environment variables and exec

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,10 +6,12 @@ eval /home/pinselimo/anaconda3/bin/conda "shell.fish" "hook" $argv | source
 alias rm='rm -i'
 alias ipy='ipython'
 alias dotfiles='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+alias android-studio='STUDIO_JDK=/usr/lib/jvm/java-11-openjdk-amd64/ /opt/android-studio/bin/studio.sh'
 
 set -x MOZ_ENABLE_WAYLAND 1
 set -x TF_CPP_MIN_LOG_LEVEL 2
 set -x JAVA_HOME "/usr"
+set -x _JAVA_AWT_WM_NONREPARENTING 1
 
 fish_add_path /home/pinselimo/.local/bin/
 fish_add_path /home/pinselimo/.cabal/bin/
