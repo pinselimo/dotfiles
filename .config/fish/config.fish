218ce87ec1e1dbe8852af4fa7767d80bae74a4f7
Simon Plakolb <s.plakolb@gmail.com>
2021-07-06 15:29:19 +0200

    fish: Adjust fish to primary NixOS usage

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,21 +1,22 @@
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /home/pinselimo/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
-
-alias rm='rm -i'
+# Set Aliases
+alias rm='rm -I'
 alias ipy='ipython'
-alias dotfiles='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+alias dotfiles='/usr/bin/env git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+alias nix-shell='nix-shell --run fish'
 alias android-studio='STUDIO_JDK=/usr/lib/jvm/java-11-openjdk-amd64/ /opt/android-studio/bin/studio.sh'
+alias se='TERM=xterm sudo -e'
 
-set -x MOZ_ENABLE_WAYLAND 1
+# Set Tensorflow log level lower 
 set -x TF_CPP_MIN_LOG_LEVEL 2
-set -x JAVA_HOME "/usr"
-set -x _JAVA_AWT_WM_NONREPARENTING 1
 
+# Adjust path
 fish_add_path /home/pinselimo/.local/bin/
-fish_add_path /home/pinselimo/.cabal/bin/
-fish_add_path /home/pinselimo/.cargo/bin/
 fish_add_path /home/pinselimo/.dynamic-colors/bin/
-fish_add_path /home/pinselimo/miniconda3/bin/
 fish_add_path /home/pinselimo/.nix-profile/bin/
+
+# What
+thefuck --alias | source
+
+if test -n "$DESKTOP_SESSION"
+    set -x (gnome-keyring-daemon --start | string split "=")
+end
