f1e309c7ae2d32cf4196e0f57f8c0ce6457bc137
Simon Plakolb <s.plakolb@gmail.com>
2021-12-11 00:19:48 +0100

    waybar: Always show input language

--- .config/waybar/scripts/keyboard.fish
+++ .config/waybar/scripts/keyboard.fish
@@ -1,6 +1,6 @@
 #! /usr/bin/env fish
 begin
-    set query (swaymsg -t get_inputs | grep 'xkb_active_layout_name' | awk -F: '{ print $2 }' | head -1)
+    set query (swaymsg -t get_inputs | grep 'xkb_active_layout_name' | awk -F: '{ print $2 }' | head -2 | tail -1)
 
     switch $query;
         case " \"German\","
