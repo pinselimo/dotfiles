209255e4b1c9eb58091dd66ee4d92a9b9356b1ef
Simon Plakolb <s.plakolb@gmail.com>
2022-04-25 00:21:46 +0200

    nix: Add custom waybar repo to flake inputs

--- nix/flake.nix
+++ nix/flake.nix
@@ -10,6 +10,10 @@
       url = "path:/home/pinselimo/vim/vim-monkey-c";
     };
 
+    pinselimo-waybar = {
+      flake = false;
+      url = "github:pinselimo/Waybar?rev=da174612c5364528ce305c32e1b6c6be4c7ed6bf";
+    };
   };
 
   outputs = { self, nixpkgs, ... }@attrs: {
