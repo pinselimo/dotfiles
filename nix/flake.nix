ed33518a22190f7fb1920f786656869ec76f06f5
Simon Plakolb <s.plakolb@gmail.com>
2022-07-18 09:36:18 +0200

    nix: Use nixos-unstable channel

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,8 +2,11 @@
   description = "Pinselimo's flake based system configuration";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.05";
-    #nixpkgs-unstable.url =  "nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    #nixpkgs-stable.url =  "nixpkgs/nixos-22.05";
+
+    #neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    #neovim-nightly-overlay.inputs.neovim-flake.url = "github:neovim/neovim/v0.7.0?dir=contrib";
 
     vim-monkey-c = {
       flake = false;
@@ -21,6 +24,11 @@
       system = "x86_64-linux";
       specialArgs = attrs;
       modules = [ ./configuration.nix ];
+    };
+    nixosConfigurations.bootable = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = attrs;
+      modules = [ ./iso.nix ];
     };
   };
 }
