678d34290858021a1c816d22a20e2ad3ea428ba9
Simon Plakolb <s.plakolb@gmail.com>
2022-08-31 15:12:24 +0200

    network: Encrypt dns queries using dnscrypt-proxy2

--- nix/services.nix
+++ nix/services.nix
@@ -60,6 +60,39 @@
     # openssh.enable = true;
     gnome.gnome-keyring.enable = true;
 
+    dnscrypt-proxy2 = {
+      enable = true;
+      settings = {
+        ipv6_servers = true;
+        require_dnssec = true;
+
+        sources.public-resolvers = {
+          urls = [
+            "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+            "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+            "https://ipv6.download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+          ];
+          cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        };
+
+        sources.relays = {
+          urls = [
+            "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/relays.md"
+            "https://download.dnscrypt.info/resolvers-list/v3/relays.md"
+            "https://ipv6.download.dnscrypt.info/resolvers-list/v3/relays.md"
+          ];
+          cache_file = "/var/lib/dnscrypt-proxy2/relays.md";
+          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        };
+        server_names = [ "cloudflare-ipv6" "cloudflare" "google-ipv6" "google" ];
+      };
+    };
+
     flatpak.enable = true; # for the usual corporate shit like zoom-us, ms-teams etc.
+  };
+
+  systemd.services.dnscrypt-proxy2.serviceConfig = {
+    StateDirectory = "dnscrypt-proxy";
   };
 }
