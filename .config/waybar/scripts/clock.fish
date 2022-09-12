08f68a113fddeb2a3f065ba3a29eeecb503b3665
Simon Plakolb <s.plakolb@gmail.com>
2022-09-12 10:21:55 +0200

    waybar: Remove legacy scripts

--- .config/waybar/scripts/clock.fish
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env fish
-begin
-    set time (date '+%H : %M : %S')
-    set day  (date '+%A %d. %B %Y')
-
-    echo -e "{\"text\":\""$time"\", \"alt\":\""$day"\", \"tooltip\":\""$day"\"}"
-end
