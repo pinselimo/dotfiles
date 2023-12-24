336d219021ea10315aeb096ba4bb3108cae90b9f
Simon Plakolb <s.plakolb@gmail.com>
2023-12-24 15:44:54 +0100

    neovim: Add function prototype for zotero citing

--- nix/neovim.nix
+++ nix/neovim.nix
@@ -72,6 +72,17 @@
         autocmd FileType markdown setlocal complete+=kspell
         autocmd FileType latex setlocal complete+=kspell
         autocmd FileType gitcommit setlocal complete+=kspell
+
+        function! ZoteroCite()
+          " pick a format based on the filetype (customize at will)
+          let format = &filetype =~ '.*tex' ? 'citep' : 'pandoc'
+          let api_call = 'http://127.0.0.1:23119/better-bibtex/cayw?format='.format.'&brackets=1'
+          let ref = system('curl -s '.shellescape(api_call))
+          return ref
+        endfunction
+
+        noremap <leader>z "=ZoteroCite()<CR>p
+        inoremap <C-z> <C-r>=ZoteroCite()<CR>
         '';
 
       packages.nix.start = with pkgs.vimPlugins; [
